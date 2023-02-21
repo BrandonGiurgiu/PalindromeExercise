3a7062762b7aa816b2be22014bffce0829a46228
bgiurgiu7@gmail.com <bgiurgiu7@gmail.com>
2023-02-20 19:01:48 -0700

    Completed Exercise 2

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public bool IsAPalindrome (string word)
+		{
+			var reversedWord = word.Reverse();
+
+			return word.SequenceEqual(reversedWord);
+			
+        }   
+		    
+				
+	}
+}
+
